991dfe52a6f33df1d0f8636c900aa0f81f6a9372
Kate Aubrey Cellan <kateaubrey.cellan@nabepero.co.jp>
2024-01-05 15:21:42 +0800

    Inserting data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create/{id}/{title}/{body}', function($id, $title, $body) {
+
+    $user = User::findOrFail($id);
+    $post = new Post(['title'=>$title, 'body'=>$body]);
+    $result = $user->posts()->save($post);
+    return $result ? true : false;
+
 });
