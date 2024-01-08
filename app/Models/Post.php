25a253976a8c5e0626a3a3b62529fe8c88e04572
Kate Aubrey Cellan <kateaubrey.cellan@nabepero.co.jp>
2024-01-08 10:14:59 +0800

    Setting up relationships and mass assignment

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function tags()
+    {
+        return $this->morphToMany('App\Models\Tag', 'taggable');
+    }
+
 }
--- app/Models/Video.php
+++ app/Models/Video.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function tags()
+    {
+        return $this->morphToMany('App\Models\Tag', 'taggable');
+    }
+
 }
