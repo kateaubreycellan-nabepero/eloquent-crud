e1d1bd03211b963eb96f40f11666ea0256db57a5
Kate Aubrey Cellan <kateaubrey.cellan@nabepero.co.jp>
2024-01-08 10:40:04 +0800

    Inserting data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\Taggable;
+use App\Models\User;
+use App\Models\Video;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +20,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('create/{tag_id1}/{tag_id2}/{post_title}/{video_title}', function($tag_id1, $tag_id2, $post_title, $video_title) {
+
+    $post = Post::create(['name'=>$post_title]);
+    $tag = Tag::findOrFail($tag_id1);
+    $result1 = $post->tags()->save($tag);
+    // echo "1st: $result1<br>";
+
+    $video = Video::create(['name'=>$video_title]);
+    $tag2 = Tag::findOrFail($tag_id2);
+    $result2 = $video->tags()->save($tag2);
+    // echo "2nd: $result2<br>";
+
+    return "$result1 | $result2";
+
 });
