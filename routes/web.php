cf9547a356d221fecb8bd304a9734945771a5419
Kate Aubrey Cellan <kateaubrey.cellan@nabepero.co.jp>
2024-01-08 10:54:15 +0800

    Reading and updating data

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('create/{tag_id1}/{tag_id2}/{post_title}/{video_title}', function($tag_id1, $tag_id2, $post_title, $video_title) {
+Route::get('/create/{tag_id1}/{tag_id2}/{post_title}/{video_title}', function($tag_id1, $tag_id2, $post_title, $video_title) {
 
     $post = Post::create(['name'=>$post_title]);
     $tag = Tag::findOrFail($tag_id1);
@@ -35,5 +35,36 @@ Route::get('create/{tag_id1}/{tag_id2}/{post_title}/{video_title}', function($ta
     // echo "2nd: $result2<br>";
 
     return "$result1 | $result2";
+
+});
+
+Route::get('/read/{post_id}', function($post_id) {
+
+    $post = Post::findOrFail($post_id);
+
+    foreach ($post->tags as $tag)
+    {
+        echo $tag;
+    }
+
+});
+
+Route::get('/update/{post_id}', function($post_id) {
+
+    $post = Post::findOrFail($post_id);
+
+    // foreach ($post->tags as $tag)
+    // {
+    //     $result = $tag->where('name', '=', 'PHP')->update(['name'=>'Updated PHP']);
+    // }
+
+    // return $result;
+
+    $tag = Tag::findOrFail(3);
+
+    // Alternative methods
+    $post->tags()->save($tag);
+    // $post->tags()->attach($tag);
+    // $post->tags()->sync([1,3]);
 
 });
